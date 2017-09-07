82c15af5138ef44ad8f679144911d3cf56e8007c
Nicolas Aguirre <nick.aguirre.16@gmail.com>
2017-09-06 22:29:50 -0300

    Primer comentario

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+	public GameObject indicador;
+	Vector2 target;
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Movimiento clickeando el raton
+		if (Input.GetMouseButtonDown (0)) {
+			//Buscamos el click respecto a la escena, target seria la posicion final
+			target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			RaycastHit2D hitSuelo=Physics2D.Raycast(target,Vector2.zero);
+			Debug.Log ("Posicion destino: "+target);
+			Debug.Log ("Ray en 2D: "+hitSuelo.collider);
+
+		}
+
+		//Movemos el objeto hacia el punto de destino, cada Update se movera hasta llegar a destino
+		transform.position = Vector2.MoveTowards (transform.position,target,speed*Time.deltaTime);
+		indicador.transform.position = target;
+		Debug.DrawLine (transform.position, target, Color.green);
+	}
+}
