208e2a6be2ac1336efb3c8d7c4340c248a0ebccd
Nicolas Aguirre <nick.aguirre.16@gmail.com>
2017-09-08 19:31:22 -0300

    Se corrigieron todos lo errores anteriores. Hay que comenzar a trabajar con las rutas de navegacion.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,51 +6,69 @@ public class Move : MonoBehaviour {
 
 	public float speed;
 	public GameObject indicador;
-	Vector2 target;
-	Vector2 inicio;
-	Vector2 newPosition;
-	bool moving;
+	public GameObject player;
+	public LayerMask notToHit;
 
+	bool moving;
+	Vector2 target;
 
 	// Use this for initialization
 	void Start () {
-		target=transform.position;
 		moving = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-//		//Movimiento clickeando el raton
 //		if (Input.GetMouseButtonDown (0)) {
-//			inicio = target;
-//			//Buscamos el click respecto a la escena, target seria la posicion final
-//			target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-//			RaycastHit2D hitSuelo=Physics2D.Raycast(target,inicio);
-//			Debug.Log ("Posicion destino: "+target+" Inicio: "+inicio);
-//			Debug.Log ("Ray en 2D: "+hitSuelo.collider.tag);
+//			RaycastHit2D hit;
 //
+//			inicio = target;
+//			target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+//			hit = Physics2D.Raycast(target,inicio-target,Vector2.Distance(transform.position,target));
+//
+//			if (hit.collider!=null) {
+//				//Asignamos nueva posicion
+//				newPosition = hit.point;
+//				indicador.transform.position = hit.point;
+//				if (hit.collider.CompareTag ("Objeto")) {
+//					moving = false;
+//					indicador.transform.position = hit.point;
+//					Debug.Log ("Hay un obstaculo");
+//				} else {
+//					moving = true;
+//				}
+//			} else {
+//				moving = true;
+//			}
 //		}
 //
-//		//Movemos el objeto hacia el punto de destino, cada Update se movera hasta llegar a destino
-//		transform.position = Vector2.MoveTowards (transform.position,target,speed*Time.deltaTime);
-//		indicador.transform.position = target;
-//		Debug.DrawLine (transform.position, target, Color.green);
+//		if(moving==true){
+//			transform.position = Vector2.MoveTowards (transform.position,target,speed*Time.deltaTime);
+//			Debug.DrawLine (transform.position, target, Color.green);
+//			if(Vector2.Distance(transform.position,newPosition)<0.1f){
+//				moving = false;
+//			}
+//		}
+
+		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+		//Se puede asignar un objeto "Raton" y asignarlo en vez de asumir que raton es el propietario de este script
+		Vector2 originPosition = new Vector2 (player.transform.position.x, player.transform.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (originPosition, mousePosition - originPosition,Vector2.Distance(originPosition,mousePosition));
+
+		Debug.DrawLine (originPosition, mousePosition, Color.green);
+		indicador.transform.position = mousePosition;
+
+		//Debug.Log (hit.collider.tag);
+		if (hit.collider != null) {
+			Debug.DrawLine (hit.point,mousePosition , Color.red);
+		}
 
 		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit2D hit;
-
-			inicio = target;
-			target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			hit = Physics2D.Raycast(target,inicio-target,Vector2.Distance(transform.position,target));
-
-			if (hit.collider!=null) {
-				//Asignamos nueva posicion
-				newPosition = hit.point;
-				indicador.transform.position = hit.point;
-				if (hit.collider.CompareTag ("Objeto")) {
+			target = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+			if (hit.collider != null) {
+				if (hit.collider.CompareTag ("Obstaculo")) {
 					moving = false;
-					indicador.transform.position = hit.point;
 					Debug.Log ("Hay un obstaculo");
 				} else {
 					moving = true;
@@ -61,12 +79,10 @@ public class Move : MonoBehaviour {
 		}
 
 		if(moving==true){
-			transform.position = Vector2.MoveTowards (transform.position,target,speed*Time.deltaTime);
-			Debug.DrawLine (transform.position, target, Color.green);
-			if(Vector2.Distance(transform.position,newPosition)<0.1f){
+			player.transform.position = Vector2.MoveTowards (originPosition,target,speed*Time.deltaTime);
+			if(Vector2.Distance(player.transform.position,target)<0.1f){
 				moving = false;
 			}
 		}
-
 	}
 }
