63415d276f951b8121c969a067d395b73f786747
Nicolas Aguirre <nick.aguirre.16@gmail.com>
2017-09-08 19:51:55 -0300

    Limpieza de codigo 1

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,46 +12,14 @@ public class Move : MonoBehaviour {
 	bool moving;
 	Vector2 target;
 
-	// Use this for initialization
 	void Start () {
 		moving = false;
 	}
-
-	// Update is called once per frame
+		
 	void Update () {
-
-//		if (Input.GetMouseButtonDown (0)) {
-//			RaycastHit2D hit;
-//
-//			inicio = target;
-//			target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-//			hit = Physics2D.Raycast(target,inicio-target,Vector2.Distance(transform.position,target));
-//
-//			if (hit.collider!=null) {
-//				//Asignamos nueva posicion
-//				newPosition = hit.point;
-//				indicador.transform.position = hit.point;
-//				if (hit.collider.CompareTag ("Objeto")) {
-//					moving = false;
-//					indicador.transform.position = hit.point;
-//					Debug.Log ("Hay un obstaculo");
-//				} else {
-//					moving = true;
-//				}
-//			} else {
-//				moving = true;
-//			}
-//		}
-//
-//		if(moving==true){
-//			transform.position = Vector2.MoveTowards (transform.position,target,speed*Time.deltaTime);
-//			Debug.DrawLine (transform.position, target, Color.green);
-//			if(Vector2.Distance(transform.position,newPosition)<0.1f){
-//				moving = false;
-//			}
-//		}
-
+		
 		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+
 		//Se puede asignar un objeto "Raton" y asignarlo en vez de asumir que raton es el propietario de este script
 		Vector2 originPosition = new Vector2 (player.transform.position.x, player.transform.position.y);
 		RaycastHit2D hit = Physics2D.Raycast (originPosition, mousePosition - originPosition,Vector2.Distance(originPosition,mousePosition));
