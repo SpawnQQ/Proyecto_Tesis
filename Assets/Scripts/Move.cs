f8c5f20481abc8a82690721579a024f447d7dd9f
Nicolas Aguirre <nick.aguirre.16@gmail.com>
2017-09-11 03:51:29 -0300

    Se agrego colisionadoren un punto especifico, falta generar las rutas de naegacion.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,6 +12,7 @@ public class Move : MonoBehaviour {
 	Vector2 target;
 
 	void Start () {
+
 		moving = false;
 	}
 		
@@ -25,25 +26,37 @@ public class Move : MonoBehaviour {
 
 		Debug.DrawLine (originPosition, mousePosition, Color.green);
 		indicador.transform.position = mousePosition;
-
+	
 		//Debug.Log (hit.collider.tag);
 		if (hit.collider != null) {
 			Debug.DrawLine (hit.point,mousePosition , Color.red);
-			Debug.DrawLine (originPosition, hit.point, Color.blue);
+			Debug.DrawLine (originPosition, hit.point, Color.gray);
 
-			for(int i=1;i<6;i++){
-				Debug.DrawLine (originPosition,new Vector2( hit.point.x-i,hit.point.y), Color.magenta);
-				Debug.DrawLine (originPosition,new Vector2( hit.point.x+i,hit.point.y), Color.cyan);
-			}
 		}
 
 		if (Input.GetMouseButtonDown (0)) {
+
+			//Sacando un angulo
+			//Debug.Log (Mathf.Atan2 (mousePosition.y - originPosition.y,mousePosition.x - originPosition.x)*(180/Mathf.PI));
+
 			target = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
 			if (hit.collider != null) {
 				if (hit.collider.CompareTag ("Obstaculo")) {
-					NavMesh2D (originPosition,mousePosition,hit.point);
-					moving = false;
-					Debug.Log ("Hay un obstaculo");
+					
+					//Verificamos si el usuario clickea sobre el objeto, esto nos dira que es inaccesible
+					Collider2D checkHitPosition = Physics2D.OverlapPoint (mousePosition);
+
+					if (checkHitPosition != null) {
+						Debug.Log (checkHitPosition.tag);
+						//NavMesh2D (originPosition,mousePosition,hit.point);
+						moving = false;
+						Debug.Log ("Hey!, no llego a ese lugar");
+					} else {
+						Debug.Log ("Hay un obstaculo");
+						moving = false;
+						//Reemplazar moving=false, por la funcion "NavMesh2D"
+					}
+
 				} else {
 					moving = true;
 				}
@@ -63,6 +76,20 @@ public class Move : MonoBehaviour {
 	//Pegado a la pared?
 	void NavMesh2D(Vector2 originPosition,Vector2 mousePosition, Vector2 hitPoint){
 		//RaycastHit2D hit = Physics2D.Raycast (originPosition, mousePosition - originPosition,Vector2.Distance(originPosition,mousePosition));
-
+//		float angulo=Mathf.Atan2 (mousePosition.y - originPosition.y,mousePosition.x - originPosition.x)*(180/Mathf.PI);
+//		for(int i=1;i<5;i++){
+//			if (((angulo <= 45 && angulo >= 0) || (angulo >= -45 && angulo <= 0)) || ((angulo >= 135 && angulo <= 180) || (angulo <= -135 && angulo >= -180))) {
+//				//Derecha e izquierda
+//				Debug.DrawLine (originPosition,new Vector2( hit.point.x,hit.point.y-i), Color.magenta);
+//				Debug.DrawLine (originPosition,new Vector2( hit.point.x,hit.point.y+i), Color.cyan);
+//			} else {
+//				if ((angulo > 45 && angulo < 135) || (angulo < -45 && angulo > -135)) {
+//					//Arriba y abajo
+//					Debug.DrawLine (originPosition,new Vector2( hit.point.x-i,hit.point.y), Color.magenta);
+//					Debug.DrawLine (originPosition,new Vector2( hit.point.x+i,hit.point.y), Color.cyan);
+//				}
+//			}
+//
+//		}
 	}
 }
