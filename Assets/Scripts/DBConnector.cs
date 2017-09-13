5def43e8a4c967f08a0d12b687571960deb54d24
Nicolas Aguirre <nick.aguirre.16@gmail.com>
2017-09-13 07:51:24 -0300

    Se esta trabajando en la incorporacion de SQLite 3, usando .NET 2.0. Faltan las consultas respectivas a la base de datos y se soluciono el error ''`System.Data.Common.DbConnection' is defined in an assembly that is not referenced. Consider adding a reference to assembly `System.Data, Version=2.0.0.0, Culture=neutral, Sqlite unity''

---
+++ Assets/Scripts/DBConnector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite; 
+using System.Data; 
+using System;
+
+public class DBConectors : MonoBehaviour {
+
+	private SqliteConnection _conexion;
+	private SqliteCommand _command;
+	private SqliteDataReader _reader;
+
+	private string _query;
+
+	public void OpenDB(string _dbName){
+		_conexion = new SqliteConnection (_dbName);
+		_conexion.Open ();
+	}
+
+
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+	public TileType[] tileTypes;
+
+	int[,] tiles;
+
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+	void Start(){
+	
+		tiles = new int[mapSizeX, mapSizeY];
+
+		//Creacon del suelo por todo el mapeado
+		for(int x=0; x<mapSizeX;x++){
+			for(int y=0; y<mapSizeY;y++){
+				tiles [x, y] = 0;
+			}
+		}
+
+		//Creacion de obstaculos
+		tiles [4, 4] = 1;
+		tiles [5, 4] = 1;
+		tiles [6, 4] = 1;
+		tiles [7, 4] = 1;
+		tiles [8, 4] = 1;
+
+		tiles [4, 5] = 1;
+		tiles [4, 6] = 1;
+		tiles [8, 5] = 1;
+		tiles [8, 6] = 1;
+
+		GenerateMapVisual ();
+	}
+
+	//Dibujamos el terreno
+	void GenerateMapVisual(){
+		for(int x=0; x<mapSizeX;x++){
+			for(int y=0; y<mapSizeY;y++){
+				TileType tt=tileTypes[tiles[x,y]];
+
+				Instantiate (tt.titleVisualPrefab, new Vector3(x,y,0),Quaternion.identity);
+			}
+		}
+
+	}
+}
